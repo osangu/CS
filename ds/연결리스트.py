cebc42d6396cb455a6ae5074013f60bfa4f4d6e6
osangu <hhgnoon@naver.com>
2022-07-21 22:38:15 +0900

    UPDATE :: LinkedList pop

--- ds/연결리스트.py
+++ ds/연결리스트.py
@@ -31,17 +31,32 @@ class LinkedList:
     def __init__(self, init_node: Node):
         self._init_node = init_node
 
-    def add_node(self, node: Node):
+    def push(self, node: Node):
         if self._is_smallest(node.data):
             node.next_node = self._init_node
             self._init_node = node
         else:
-            before_node = self._find_next_node(node.data)
+            before_node = self._find_before_node(node.data)
             if before_node.next_node is None:
                 before_node.next_node = node
             else:
                 node.next_node = before_node.next_node
                 before_node.next_node = node
+
+    def pop(self, index):
+        if index == 0:
+            return_node = self._init_node
+            self._init_node = self._init_node.next_node
+        elif index == self.node_amount:
+            before_node = self._find_before_node_by_index(index)
+            return_node = before_node.next_node
+            before_node.next_node = None
+        else:
+            before_node = self._find_before_node_by_index(index)
+            return_node = before_node.next_node
+            before_node.next_node = before_node.next_node.next_node
+
+        return return_node
 
     def show_all_node_data(self):
         print('=========================')
@@ -51,7 +66,22 @@ class LinkedList:
             if run_node.next_node is None: break
             run_node = run_node.next_node
 
-    def _find_next_node(self, data: int):
+    @property
+    def node_amount(self):
+        cnt, run_node = 1, self._init_node
+        while True:
+            if run_node.next_node is None: return cnt
+            cnt += 1
+            run_node = run_node.next_node
+
+    def _find_before_node_by_index(self, index: int):
+        run_node = self._init_node
+        if index != 0:
+            for i in range(index - 2):  # init node 제외로 -1 해당 인덱스 전의 node니 -1
+                run_node = run_node.next_node
+        return run_node
+
+    def _find_before_node(self, data: int):
         run_node = self._init_node
         while True:
             if run_node.next_node is None: return run_node
@@ -66,23 +96,36 @@ class LinkedList:
 if __name__ == '__main__':
     linked_list = LinkedList(Node(3))
 
-    linked_list.add_node(Node(1))
+    linked_list.push(Node(1))
 
     linked_list.show_all_node_data()
 
-    linked_list.add_node(Node(5))
+    linked_list.push(Node(5))
 
     linked_list.show_all_node_data()
 
-    linked_list.add_node(Node(4))
+    linked_list.push(Node(4))
 
     linked_list.show_all_node_data()
 
-    linked_list.add_node(Node(0))
+    linked_list.push(Node(0))
 
     linked_list.show_all_node_data()
 
-    linked_list.add_node(Node(7))
+    linked_list.push(Node(7))
 
     linked_list.show_all_node_data()
 
+    print('===')
+    print(linked_list.pop(6).data)
+
+    print('===')
+    print(linked_list.pop(0).data)
+
+    print('===')
+    print(linked_list.pop(4).data)
+
+    print('===')
+    print(linked_list.pop(1).data)
+
+    linked_list.show_all_node_data()
