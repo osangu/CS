d57683095f4c00e0318c948cd65170d9e9197bb9
osangu <hhgnoon@naver.com>
2022-07-22 18:52:43 +0900

    UPDATE :: iterable LinkedList

--- ds/연결리스트.py
+++ ds/연결리스트.py
@@ -30,13 +30,26 @@ class LinkedList:
 
     def __init__(self, init_node: Node):
         self._init_node = init_node
+        self.__iter_cnt = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.__iter_cnt < self.node_amount:
+            return_node = self._find_node_by_index(self.__iter_cnt)
+            self.__iter_cnt += 1
+            return return_node
+        else:
+            self.__iter_cnt = 0
+            raise StopIteration
 
     def push(self, node: Node):
         if self._is_smallest(node.data):
             node.next_node = self._init_node
             self._init_node = node
         else:
-            before_node = self._find_before_node(node.data)
+            before_node = self._find_before_node_by_data(node.data)
             if before_node.next_node is None:
                 before_node.next_node = node
             else:
@@ -74,6 +87,13 @@ class LinkedList:
             cnt += 1
             run_node = run_node.next_node
 
+    def _find_node_by_index(self, index: int):
+        run_node = self._init_node
+        if index != 0:
+            for i in range(index):
+                run_node = run_node.next_node
+        return run_node
+
     def _find_before_node_by_index(self, index: int):
         run_node = self._init_node
         if index != 0:
@@ -81,7 +101,7 @@ class LinkedList:
                 run_node = run_node.next_node
         return run_node
 
-    def _find_before_node(self, data: int):
+    def _find_before_node_by_data(self, data: int):
         run_node = self._init_node
         while True:
             if run_node.data == data: raise DataAllReadyExistException(data)
@@ -91,43 +111,3 @@ class LinkedList:
 
     def _is_smallest(self, data) -> bool:
         return self._init_node.data > data
-
-
-if __name__ == '__main__':
-    linked_list = LinkedList(Node(3))
-
-    linked_list.push(Node(1))
-
-#    linked_list.push(Node(1))
-
-    linked_list.show_all_node_data()
-
-    linked_list.push(Node(5))
-
-    linked_list.show_all_node_data()
-
-    linked_list.push(Node(4))
-
-    linked_list.show_all_node_data()
-
-    linked_list.push(Node(0))
-
-    linked_list.show_all_node_data()
-
-    linked_list.push(Node(7))
-
-    linked_list.show_all_node_data()
-
-    print('===')
-    print(linked_list.pop(6).data)
-
-    print('===')
-    print(linked_list.pop(0).data)
-
-    print('===')
-    print(linked_list.pop(4).data)
-
-    print('===')
-    print(linked_list.pop(1).data)
-
-    linked_list.show_all_node_data()
