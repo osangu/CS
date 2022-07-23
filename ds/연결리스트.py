d71a8da28430fb349068ad124d79e52ae77f755e
osangu <hhgnoon@naver.com>
2022-07-23 12:56:40 +0900

    REFACTOR :: LinkedList

--- ds/연결리스트.py
+++ ds/연결리스트.py
@@ -1,9 +1,19 @@
 from typing import Optional
 
 
+class HeadNodeNotExistException(Exception):
+    def __init__(self):
+        super().__init__("ERROR: HEAD NODE DOESN'T EXIST")
+
+
+class NextNodeNotExistException(Exception):
+    def __init__(self, data):
+        super().__init__(f"ERROR: NO NODE FOUND WITH DATA AFTER {data}")
+
+
 class DataAllReadyExistException(Exception):
     def __init__(self, data):
-        super().__init__(data, 'ALREADY EXIST')
+        super().__init__(f'ERROR: NODE WHICH DATA IS {data} ALREADY EXIST')
 
 
 class Node:
@@ -28,26 +38,48 @@ class Node:
 
 class LinkedList:
 
-    def __init__(self, init_node: Node):
-        self._init_node = init_node
+    def __init__(self):
+        self.__initial_node = None
         self.__iter_cnt = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.__iter_cnt < self.node_amount:
-            return_node = self._find_node_by_index(self.__iter_cnt)
+        if self.node_amount > self.__iter_cnt:
+            now_node = self._find_node_by_index(self.__iter_cnt)
             self.__iter_cnt += 1
-            return return_node
+            return now_node
+
         else:
             self.__iter_cnt = 0
             raise StopIteration
 
+    @property
+    def _first_node(self) -> Node:
+        if self.__initial_node is None: raise HeadNodeNotExistException
+        return self.__initial_node
+
+    @_first_node.setter
+    def _first_node(self, node: Node):
+        if self.__initial_node is None:
+            self.__initial_node = node
+        else:
+            node.next_node = self.__initial_node
+            self.__initial_node = node
+
+    @property
+    def node_amount(self) -> int:
+        cnt, run_node = 1, self._first_node
+        while True:
+            if run_node.next_node is None: break
+            cnt += 1
+            run_node = run_node.next_node
+        return cnt
+
     def push(self, node: Node):
         if self._is_smallest(node.data):
-            node.next_node = self._init_node
-            self._init_node = node
+            self._first_node = node
         else:
             before_node = self._find_before_node_by_data(node.data)
             if before_node.next_node is None:
@@ -56,58 +88,24 @@ class LinkedList:
                 node.next_node = before_node.next_node
                 before_node.next_node = node
 
-    def pop(self, index):
-        if index == 0:
-            return_node = self._init_node
-            self._init_node = self._init_node.next_node
-        elif index == self.node_amount:
-            before_node = self._find_before_node_by_index(index)
-            return_node = before_node.next_node
-            before_node.next_node = None
-        else:
-            before_node = self._find_before_node_by_index(index)
-            return_node = before_node.next_node
-            before_node.next_node = before_node.next_node.next_node
-
-        return return_node
-
-    def show_all_node_data(self):
-        print('=========================')
-        run_node = self._init_node
+    def _find_before_node_by_data(self, data: int):
+        run_node = self._first_node
         while True:
-            print(run_node.data)
-            if run_node.next_node is None: break
+            if (run_node.next_node is None) or (run_node.next_node.data > data): break
             run_node = run_node.next_node
-
-    @property
-    def node_amount(self):
-        cnt, run_node = 1, self._init_node
-        while True:
-            if run_node.next_node is None: return cnt
-            cnt += 1
-            run_node = run_node.next_node
+        return run_node
 
     def _find_node_by_index(self, index: int):
-        run_node = self._init_node
-        if index != 0:
-            for i in range(index):
-                run_node = run_node.next_node
-        return run_node
-
-    def _find_before_node_by_index(self, index: int):
-        run_node = self._init_node
-        if index != 0:
-            for i in range(index - 2):  # init node 제외로 -1 해당 인덱스 전의 node니 -1
-                run_node = run_node.next_node
-        return run_node
-
-    def _find_before_node_by_data(self, data: int):
-        run_node = self._init_node
-        while True:
-            if run_node.data == data: raise DataAllReadyExistException(data)
-            if run_node.next_node is None: return run_node
-            if run_node.next_node.data > data: return run_node
+        run_node = self._first_node
+        for i in range(index):
             run_node = run_node.next_node
+        return run_node
 
-    def _is_smallest(self, data) -> bool:
-        return self._init_node.data > data
+    def _is_smallest(self, data: int):
+        global result
+        try:
+            result = self._first_node.data > data
+        except HeadNodeNotExistException:
+            result = True
+        finally:
+            return result
